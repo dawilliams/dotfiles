97e0ad7d39de7d567f97dac4db50ed30dd8f507c
David Williams <williamsgang1@gmail.com>
2025-06-06 17:03:19 -0500

    Add neovim options and keymaps

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -2,8 +2,31 @@ require("config.lazy")
 require("config.diagnostic")
 require("config.lsp")
 
-vim.opt.shiftwidth = 4                                      -- Set tab width to 4 spaces
-vim.opt.clipboard = "unnamedplus"                           -- Allow "p" in addition ctrl-p to paste from clipboard
+-- options
+vim.opt.clipboard = "unnamedplus" -- Allow "p" in addition ctrl-p to paste from clipboard
+vim.opt.cursorline = true         -- Show which line your cursor is on
+vim.opt.inccommand = "split"      -- Preview substitutions live, as you type!
+vim.o.undofile = true             -- Save undo history
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- keymaps
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')         -- When in normal mode("n"), clear highlights on search when pressing <Esc>
 vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>") -- When in normal mode("n"), source (run) the current file(%)
 vim.keymap.set("n", "<space>x", ":.lua<CR>")                -- When in normal mode("n"), source (run) the current line
 vim.keymap.set("v", "<space>x", ":lua<CR>")                 -- When in visual mode("v"), source (run) the selected line(s)
