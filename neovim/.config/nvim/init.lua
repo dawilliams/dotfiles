f8923bac1a2faebde2d3c96e5b952bbd054744bb
David Williams <williamsgang1@gmail.com>
2025-06-10 06:43:28 -0500

    Neovim break terminal config into seperate file.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
-require("config.lazy")
+require("config.lazy") -- lazy config must come first
 require("config.diagnostic")
 require("config.lsp")
+require("config.terminal")
 
 -- options
 vim.opt.clipboard = "unnamedplus" -- Allow "p" in addition ctrl-p to paste from clipboard
@@ -41,24 +42,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
---Terminal Config
-vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>") -- Escape terminal mode by typing esc esc.
-
--- Customize terminal options
-vim.api.nvim_create_autocmd('TermOpen', {
-  desc = 'Customize Neovim Terminal Options',
-  group = vim.api.nvim_create_augroup('custom-term-open', { clear = true }),
-  callback = function()
-    vim.opt.number = false
-    vim.opt.relativenumber = false
-  end,
-})
-
--- Configure terminal that is 15 lines tall at the bottom of the screen
-vim.keymap.set("n", "<space>st", function() -- st = small terminal
-  vim.cmd.vnew()
-  vim.cmd.term()
-  vim.cmd.wincmd("J")
-  vim.api.nvim_win_set_height(0, 15)
-end)
