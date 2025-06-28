be084a4b88c4086bda80065282dd82a6761d15cc
David Williams <dwilliams@shipt.com>
2025-06-27 19:26:29 -0500

    Refactor neovim to be like kickstart.nvim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -3,57 +3,115 @@ require("config.lsp")
 require("config.terminal")
 
 -- options
-vim.opt.undofile = true -- Save undo history
-vim.opt.autoindent = true -- Keep indentation from previous line
-vim.opt.breakindent = true -- Every wrapped line will continue visually indented
-vim.opt.smartindent = true -- Determine indention based on characters and other criteria
-vim.opt.cursorline = true -- Show which line your cursor is on
-vim.opt.number = true -- Show line numbers
-vim.opt.relativenumber = true -- Show line numbers relative to the cursor line
-vim.opt.showmode = false -- Don't show the mode, since it's already in the status line
-vim.opt.mouse = "a" -- Enable mouse mode, useful for resizing splits
-vim.opt.clipboard = "unnamedplus" -- Allow "p" in addition ctrl-p to paste from clipboard
-vim.opt.scrolloff = 10 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.ignorecase = true -- Ignore case in searches
-vim.opt.smartcase = true -- Override ignorecase if search pattern contains a capital letter
-vim.opt.splitright = true -- Open new tab to the right
-vim.opt.splitbelow = true -- Open new tab to the bottom
-vim.opt.inccommand = "split" -- Preview substitutions live, as you type!
-vim.opt.expandtab = true -- Convert tabs to spaces
-vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
-vim.opt.smarttab = true
-vim.opt.softtabstop = 4 -- How many spaces are applied when pressing tab
-vim.opt.tabstop = 4 -- How many spaces are shown per tab
-vim.opt.list = true -- Show whitespace characters
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' } -- Configure how to display whitespace characters
+-- vim.opt.autoindent = true -- Keep indentation from previous line
+-- vim.opt.smartindent = true -- Determine indention based on characters and other criteria
+-- vim.opt.expandtab = true -- Convert tabs to spaces
+-- vim.opt.shiftwidth = 4 -- Amount to indent with << and >>
+-- vim.opt.smarttab = true
+-- vim.opt.softtabstop = 4 -- How many spaces are applied when pressing tab
+-- vim.opt.tabstop = 4 -- How many spaces are shown per tab
+
+-- See `:help vim.o`
+-- NOTE: You can change these options as you wish!
+--  For more options, you can see `:help option-list`
+
+-- Make line numbers default
+vim.o.number = true
+-- You can also add relative line numbers, to help with jumping.
+--  Experiment for yourself to see if you like it!
+vim.o.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+vim.o.mouse = "a"
+
+-- Don't show the mode, since it's already in the status line
+vim.o.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.o.clipboard = "unnamedplus"
+end)
+
+-- Enable break indent
+vim.o.breakindent = true
+
+-- Save undo history
+vim.o.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.o.signcolumn = "yes"
+
+-- Decrease update time
+vim.o.updatetime = 250
+
+-- Decrease mapped sequence wait time
+vim.o.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
+vim.o.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+
+-- Preview substitutions live, as you type!
+vim.o.inccommand = "split"
+
+-- Show which line your cursor is on
+vim.o.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.o.scrolloff = 10
+
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.o.confirm = true
 
 -- keymaps
-vim.keymap.set('n', '-', '<cmd>Oil --float<CR>', { desc = 'Open Parent Directory in Oil' })       -- When in normal mode("n"), press - to open the Oil file browser
-vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>', { desc = 'Clear highlights on search' })      -- When in normal mode("n"), press <Esc> to clear highlights on search
-vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = 'Source the current file' }) -- When in normal mode("n"), source (run) the current file(%)
-vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = 'Source the current line' })                -- When in normal mode("n"), source (run) the current line
-vim.keymap.set("v", "<space>x", ":lua<CR>", { desc = 'Source the selected line{s)' })             -- When in visual mode("v"), source (run) the selected line(s)
+--  See `:help vim.keymap.set()`
+vim.keymap.set("n", "-", "<cmd>Oil --float<CR>", { desc = "Open Parent Directory in Oil" }) -- When in normal mode("n"), press - to open the Oil file browser
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>", { desc = "Clear highlights on search" }) -- When in normal mode("n"), press <Esc> to clear highlights on search
+vim.keymap.set("n", "<space><space>x", "<cmd>source %<CR>", { desc = "Source the current file" }) -- When in normal mode("n"), source (run) the current file(%)
+vim.keymap.set("n", "<space>x", ":.lua<CR>", { desc = "Source the current line" }) -- When in normal mode("n"), source (run) the current line
+vim.keymap.set("v", "<space>x", ":lua<CR>", { desc = "Source the selected line{s)" }) -- When in visual mode("v"), source (run) the selected line(s)
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
 
 -- auto commands
 --- Hightlight when yanking (copying) text
 --- Try it with `yap` in normal mode
 --- See `:help vim.highlight.on_yank()`
-vim.api.nvim_create_autocmd('TextYankPost', {
-  desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
+vim.api.nvim_create_autocmd("TextYankPost", {
+  desc = "Highlight when yanking (copying) text",
+  group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
 
 --- Ennable treesitter highlighting
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'go', 'lua', 'json' },
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "go", "lua", "json" },
   callback = function()
     -- syntax highlighting, provided by Neovim
     vim.treesitter.start()
     -- folds, provided by Neovim
-    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+    vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
     -- indentation, provided by nvim-treesitter
     vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
   end,
