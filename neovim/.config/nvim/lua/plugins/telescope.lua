d7432e84b17e56583de51c09f6b025db2d8f8bd4
David Williams <dwilliams@shipt.com>
2025-09-19 14:11:32 -0500

    neovim: syntax format refactor

--- neovim/.config/nvim/lua/plugins/telescope.lua
+++ neovim/.config/nvim/lua/plugins/telescope.lua
@@ -1,35 +1,36 @@
 return {
   {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
+    "nvim-telescope/telescope.nvim",
+    enabled = false,
+    tag = "0.1.8",
     dependencies = {
-      'nvim-lua/plenary.nvim',
+      "nvim-lua/plenary.nvim",
       -- significantly improve sorting performance
-      { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
     },
     config = function()
-      require('telescope').setup {
+      require("telescope").setup({
         pickers = {
           find_files = {
-            theme = "ivy" -- ivy pulls overlay from the bottom of the screen
-          }
+            theme = "ivy", -- ivy pulls overlay from the bottom of the screen
+          },
         },
         extensions = {
-          fzf = {} -- significantly improve sorting performance
-        }
-      }
+          fzf = {}, -- significantly improve sorting performance
+        },
+      })
 
-      require('telescope').load_extension('fzf')                                                       -- significantly improve sorting performance
+      require("telescope").load_extension("fzf") -- significantly improve sorting performance
 
-      vim.keymap.set("n", "<space>fh", require('telescope.builtin').help_tags, { desc = 'find help' }) -- fh = find help
-      vim.keymap.set("n", "<space>ff", require('telescope.builtin').find_files)                        -- ff = find directory
+      vim.keymap.set("n", "<space>fh", require("telescope.builtin").help_tags, { desc = "find help" }) -- fh = find help
+      vim.keymap.set("n", "<space>ff", require("telescope.builtin").find_files) -- ff = find directory
       -- en = edit neovim
       vim.keymap.set("n", "<space>en", function()
-        require('telescope.builtin').find_files {
-          cwd = vim.fn.stdpath("config") -- cwd = current working directory
-        }
+        require("telescope.builtin").find_files({
+          cwd = vim.fn.stdpath("config"), -- cwd = current working directory
+        })
       end)
-      require "config.telescope.multigrep".setup() -- multigrep config located at nvim/lua/config/telescope/multigrep.lua
-    end
-  }
+      require("config.telescope.multigrep").setup() -- multigrep config located at nvim/lua/config/telescope/multigrep.lua
+    end,
+  },
 }
